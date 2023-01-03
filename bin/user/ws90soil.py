35ce734e5868da8db3fa9e7189549838ffb29b75
erudita <49094080+erudita@users.noreply.github.com>
2023-01-03 16:26:48 +1100

    Update ws90soil.py

--- bin/user/ws90soil.py
+++ bin/user/ws90soil.py
@@ -1,10 +1,12 @@
-import schemas.wview
+import schemas.wview_extended
 import weewx.units
 
-schema_with_ws90 = schemas.wview.schema + [('soilMoist5', 'REAL')]
-schema_with_ws90 = schemas.wview.schema + [('soilMoist6', 'REAL')]
-schema_with_ws90 = schemas.wview.schema + [('soilMoist7', 'REAL')]
-schema_with_ws90 = schemas.wview.schema + [('soilMoist8', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('soilMoist5', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('soilMoist6', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('soilMoist7', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('soilMoist8', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('rainPiezo', 'REAL')]
+schema_with_ws90 = schemas.wview.schema_extended + [('rainPiezoRate', 'REAL')]
 
 weewx.units.obs_group_dict['soilMoist1'] = 'group_percent'
 weewx.units.obs_group_dict['soilMoist2'] = 'group_percent'
