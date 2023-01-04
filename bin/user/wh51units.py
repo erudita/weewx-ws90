c4bc6a8846c0e4f4f4ec6ae03b6fe806d9f90e07
erudita <49094080+erudita@users.noreply.github.com>
2023-01-04 13:28:32 +1100

    Create wh51units.py

---
+++ bin/user/wh51units.py
@@ -0,0 +1,13 @@
+## The ecowitt WH51 reports moisture as a percentage
+##
+
+import weewx.units
+
+weewx.units.obs_group_dict['soilMoist1'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist2'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist3'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist4'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist5'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist6'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist7'] = 'group_percent'
+weewx.units.obs_group_dict['soilMoist8'] = 'group_percent'
