026d1d557c5aefad90728d35b26c0f92162d5aee
erudita <49094080+erudita@users.noreply.github.com>
2023-01-04 11:55:59 +1100

    Update install.py

--- install.py
+++ install.py
@@ -3,26 +3,40 @@
 
 import configobj
 from weecfg.extension import ExtensionInstaller
-from six.moves import StringIO
+
+try:
+    # Python 3
+    from io import StringIO
+except ImportError:
+    # Python 2
+    from StringIO import StringIO
+    
+# WeeWX imports
+import weewx
+
+from setup import ExtensionInstaller
+
+REQUIRED_VERSION = "4.5.0"
+
 
 # define our config as a multiline string so we can preserve comments
-ws90_config = """
+ws90_config = u"""
 
 [Accumulator]
     [[rainPiezo]]
         extractor = sum
 
 """
-
+# obtain our config string as a configobj dict
 ws90_dict = configobj.ConfigObj(StringIO(ws90_config))
 
 def loader():
-    return WS90Installer()
+    return WSInstaller()
 
-class WS90Installer(ExtensionInstaller):
+class WSInstaller(ExtensionInstaller):
     def __init__(self):
-        super(WS90Installer, self).__init__(
-            version="0.1.1",
+        super(WSInstaller, self).__init__(
+            version="0.1.3",
             name='ws90',
             description='schema updates for ws90 soil',
             author="Erudita",
