6c7212c9b318cccf1e1db6343731072d53bdd606
erudita <49094080+erudita@users.noreply.github.com>
2023-01-04 15:40:03 +1100

    Update wh51units.py

--- bin/user/wh51units.py
+++ bin/user/wh51units.py
@@ -3,11 +3,22 @@
 
 import weewx.units
 
-weewx.units.obs_group_dict['soilMoist1'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist2'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist3'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist4'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist5'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist6'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist7'] = 'group_percent'
-weewx.units.obs_group_dict['soilMoist8'] = 'group_percent'
+class WH51(StdService):
+    """WeeWX service for initialising data for WH51 compatibility (soil moisture as a percentage """
+
+    def __init__(self, engine, config_dict):
+        # Initialize my superclass:
+        super(WH51, self).__init__(engine, config_dict)
+
+        
+        weewx.units.obs_group_dict['soilMoist1'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist2'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist3'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist4'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist5'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist6'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist7'] = 'group_percent'
+        weewx.units.obs_group_dict['soilMoist8'] = 'group_percent'
+        
+
+        loginf("Setup WH51 obs_group")
