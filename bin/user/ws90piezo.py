f30c868c255276e09bd20e34d55f923d0545a3dd
erudita <49094080+erudita@users.noreply.github.com>
2023-01-05 11:48:49 +1100

    Update ws90piezo.py

--- bin/user/ws90piezo.py
+++ bin/user/ws90piezo.py
@@ -1,8 +1,22 @@
-import schemas.wview_extended
+## The ecowitt WH51 reports moisture as a percentage
+##
+
+import weewx
 import weewx.units
+from weewx.wxengine import StdService
 
-schema_with_ws90 = schemas.wview.schema_extended + [('rainPiezo', 'REAL')]
-schema_with_ws90 = schemas.wview.schema_extended + [('rainPiezoRate', 'REAL')]
+import weeutil.logger
+import logging
+log = logging.getLogger(__name__)
 
-weewx.units.obs_group_dict['rainPiezoRate'] = 'group_rainrate'
-weewx.units.obs_group_dict['rainPiezo'] = 'group_rain'
+class WS90(StdService):
+    """WeeWX service for initialising data for WH51 compatibility (soil moisture as a percentage """
+
+    def __init__(self, engine, config_dict):
+        # Initialize my superclass:
+        super(WS90, self).__init__(engine, config_dict)
+
+        weewx.units.obs_group_dict['rainPiezoRate'] = 'group_rainrate'
+        weewx.units.obs_group_dict['rainPiezo'] = 'group_rain'
+        
+        log.debug("Setup WS90 obs_group_dict rainPiezo")
