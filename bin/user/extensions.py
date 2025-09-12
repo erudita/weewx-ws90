cb054a881f5f6fb70a94e2cbec0e290f0a7828ca
erudita <49094080+erudita@users.noreply.github.com>
2025-09-12 18:40:28 +1000

    extensions for WS90

--- bin/user/extensions.py
+++ bin/user/extensions.py
@@ -1,1 +1,79 @@
+import weewx.units
+# units not delivered by the GW2000
+# typical weewx.conf sections are
+# [Accumulator]
+#    [[rainPiezo]] 
+#        extractor = sum
+#    [[irrigationVolume1]] 
+#        extractor = sum
+#    [[irrigationVolume2]]
+#        extractor = sum
+
+# for the WFC01 (irrigation).
+# I include here for convenience
+weewx.units.obs_group_dict['irrigationVolum1'] = 'group_volume'
+weewx.units.obs_group_dict['irrigationVolum2'] = 'group_volume'
+
+# units delivered by the GW2000
+
+# for the WS90 (all-in-1)
+
+# Voltages are reported, so ...
+# option 1 was to change groups of existing measure, e.g.
+#    change rainBatteryStatus to group_volt
+# option 2 create new measures, e.g.
+#    rainPiezeoBattery - group_volt
+## weewx.units.obs_group_dict['rainPiezoBatteryVoltage'] = 'group_volt'
+## weewx.units.obs_group_dict['rainBatteryVoltage'] = 'group_volt'
+# rain is WH40, piezo rain, wind is WS90, option 2
+
+weewx.units.obs_group_dict['rainBatteryStatus'] = 'group_volt'
+weewx.units.obs_group_dict['hailBatteryStatus'] = 'group_volt'
+weewx.units.obs_group_dict['WS90BatteryStatus'] = 'group_volt'
+weewx.units.obs_group_dict['WH40BatteryStatus'] = 'group_volt'
+
+# This configuration sets "piezo" types, which are used in the loop 
+# for clarity.
+# they are assigned to database types for archive records in the 
+# [StdCalibrate] [[Corrections]] section
+
+weewx.units.obs_group_dict['rainPiezo'] = 'group_rain'
+weewx.units.obs_group_dict['yearRainPiezo'] = 'group_rain'
+weewx.units.obs_group_dict['rainPiezoRate'] = 'group_rain'
+weewx.units.obs_group_dict['eventRain'] = 'group_rain'
+weewx.units.obs_group_dict['eventRainPiezo'] = 'group_rain'
+
+# some addons to be used in the loop and MQTT generation but not database
+weewx.units.obs_group_dict['rain24'] = 'group_rain'
+weewx.units.obs_group_dict['rainPiezo24'] = 'group_rain'
+weewx.units.obs_group_dict['monthRainPiezo'] = 'group_rain'
+
+# WS 90 gives options for wind direction.
+weewx.units.obs_group_dict['windDirAvg10m'] = 'group_direction'
+weewx.units.obs_group_dict['windDirInstant'] = 'group_direction'
+
+# WS 90 provides summary fields
+weewx.units.obs_group_dict['windGustMax'] = 'group_speed'
+
+# for the WH51 (soil moisture)
+weewx.units.obs_group_dict['soilMoist1']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist2']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist3']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist4']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist5']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist6']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist7']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist8']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist9']		= 'group_percent'
+weewx.units.obs_group_dict['soilMoist10']		= 'group_percent'
+weewx.units.obs_group_dict['soilBatteryVoltage1']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage2']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage3']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage4']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage5']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage6']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage7']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage8']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage9']	= 'group_volt'
+weewx.units.obs_group_dict['soilBatteryVoltage10']	= 'group_volt'
 
