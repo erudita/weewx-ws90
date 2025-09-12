93400663bfda4d43627bf14c372df54155c9ffda
erudita <49094080+erudita@users.noreply.github.com>
2025-09-12 22:06:40 +1000

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,1 +1,30 @@
+#!/usr/bin/env sh
 
+WEEWX_ROOT=/etc/weewx
+WEEWX_CONF_DIST=$WEEWX_ROOT/weewx.conf
+CONFIGURATION_MOUNTPOINT=/data
+WEEWX_CONF_LIVE=$CONFIGURATION_MOUNTPOINT/etc/weewx.conf
+
+if [ -f $WEEWX_CONF_LIVE ];
+then
+	echo "Using Live Configuration"
+	WEEWX_CONF=$WEEWX_CONF_LIVE
+else
+	echo "Using Distribution Configuration"
+	WEEWX_CONF=$WEEWX_CONF_DIST 
+fi
+
+if [ "$#" -ge 1 ] && [ "$1" = "--test" ];
+then
+	/bin/sh
+        exit
+else
+	echo "num. args is $#"
+fi
+
+echo "WEEWX_CONF=$WEEWX_CONF"
+
+while true; do
+  weewxd $WEEWX_CONF > /dev/stdout
+  sleep 60
+done
